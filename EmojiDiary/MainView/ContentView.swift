d1f6fd7cd899c57ab45c139cbe68d425b01fcaa3
algamza246 <algamza246@gmail.com>
2025-08-12 17:30:54 +0900

    feat: add DatePicker

---
+++ EmojiDiary/MainView/ContentView.swift
@@ -0,0 +1,44 @@
+//
+//  ContentView.swift
+//  EmojiDiary
+//
+//  Created by 서정원 on 8/12/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var date = Date()
+    
+    var body: some View {
+        TabView {
+            // 첫 번째 탭
+            VStack() {
+                Text("Project Name")
+                    .font(.headline)
+                    .fontWeight(.bold)
+                
+                DatePicker("", selection: $date, displayedComponents: [.date])
+                    .datePickerStyle(.graphical)
+                    .environment(\.locale, Locale(identifier: "ko"))
+                    .padding()
+                Spacer()
+            }
+                .tabItem {
+                    Image(systemName: "calendar")
+                    Text("달력")
+                }
+            
+            // 두 번째 탭
+            Text("두 번째 화면")
+                .tabItem {
+                    Image(systemName: "chart.pie")
+                    Text("통계")
+                }
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
