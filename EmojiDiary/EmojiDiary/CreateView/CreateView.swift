82c819cd5df1afbbd11a6b6641ef60d5d2be1677
EunYoung <e.hazel.w@icloud.com>
2025-08-13 13:47:21 +0900

    상단바 수정

--- EmojiDiary/EmojiDiary/CreateView/CreateView.swift
+++ EmojiDiary/EmojiDiary/CreateView/CreateView.swift
@@ -22,6 +22,7 @@ struct CreateView: View {
               Image(systemName:"chevron.backward")
             }
           }
+          
           ToolbarItem(placement: .principal){
             // 선택한 날짜
             Text(getDate)
@@ -55,6 +56,9 @@ struct CreateView: View {
               .background(.yellow.opacity(0.1))
           )
           .font(.body)
+        //FocusState
+         
+        
         
         
         //가이드 텍스트 표시
