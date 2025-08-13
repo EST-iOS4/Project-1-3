16ae4df24404e66224186a88f96fed3eb907d5be
algamza246 <algamza246@gmail.com>
2025-08-13 17:47:59 +0900

    설정 버튼을 통한 SettingView로 화면 전환

--- EmojiDiary/EmojiDiary/MainView/ContentView.swift
+++ EmojiDiary/EmojiDiary/MainView/ContentView.swift
@@ -2,55 +2,57 @@ import SwiftUI
 
 
 struct ContentView: View {
-    @State private var date = Date()
+    @State private var getDate = Date()
     
     var body: some View {
-        // 첫 번째 화면
-        TabView {
-            // 첫 번째 탭
-            Text("첫 번째 화면")
-            VStack() {
-                ZStack{
-                    Text("Project Name") //Project 제목
-                        .font(.headline)
-                        .padding(.horizontal)
-                    
-                    HStack{
-                        Spacer()
-                        
-                        Button(action: {
-                            // 설정 버튼 클릭 시 동작
-                        }) {
-                            Image(systemName: "gearshape")
-                                .imageScale(.large)
-                                .padding(.horizontal)
+        NavigationStack {
+            // 첫 번째 화면
+            TabView {
+                // 첫 번째 탭
+                Text("첫 번째 화면")
+
+                VStack() {
+                    ZStack{
+                        Text("Project Name") //Project 제목
+                            .font(.headline)
+                            .padding(.horizontal)
+                        HStack{
+                            Spacer()
+                            
+                            
+                            NavigationLink(destination: SettingView()) {
+                                Image(systemName: "gearshape")
+                                    .imageScale(.large)
+                                    .padding(.horizontal)
+                            }
                         }
                     }
+                    
+                    
+                    DatePicker("", selection: $getDate, in: ...Date(), displayedComponents: [.date]) //달력
+                        .datePickerStyle(.graphical)
+                        .environment(\.locale, Locale(identifier: "ko")) //yyyy mm 부분 영어 -> 한글
+                        .padding(.horizontal)
+                    
+                    Spacer()
                 }
+                    .tabItem {
+                        Image(systemName: "calendar")
+                        Text("달력")
+                    }
                 
-                
-                DatePicker("", selection: $date, in: ...Date(), displayedComponents: [.date]) //달력
-                    .datePickerStyle(.graphical)
-                    .environment(\.locale, Locale(identifier: "ko")) //yyyy mm 부분 영어 -> 한글
-                    .padding(.horizontal)
-                Spacer()
+                    
+                // 두 번째 탭
+                Text("두 번째 화면")
+                    .tabItem {
+                        Image(systemName: "chart.pie")
+                        Text("통계")
+                        
+                        
+                    }
             }
-            
-                .tabItem {
-                    Image(systemName: "calendar")
-                    Text("달력")
-                }
-            
-                
-            // 두 번째 탭
-            Text("두 번째 화면")
-                .tabItem {
-                    Image(systemName: "chart.pie")
-                    Text("통계")
-                    
-                    
-                }
         }
+
     }
 }
 
---
+++ EmojiDiary/EmojiDiary/SettingView/SettingView.swift
@@ -0,0 +1,14 @@
+//
+//  SettingView.swift
+//  EmojiDiary
+//
+//  Created by 강지원 on 8/13/25.
+//
+
+import SwiftUI
+
+struct SettingView: View {
+    var body: some View {
+        Text("Setting View")
+    }
+}
