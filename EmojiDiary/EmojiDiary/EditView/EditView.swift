fe2591d9791cbe7c870d8aaf99dc8967a3a56892
EunYoung <e.hazel.w@icloud.com>
2025-08-13 17:53:53 +0900

    rebase 문제

--- EmojiDiary/EmojiDiary/EditView/EditView.swift
+++ EmojiDiary/EmojiDiary/EditView/EditView.swift
@@ -9,79 +9,91 @@ import SwiftUI
 struct EditView: View {
   @State private var comment: String = "" //일기 본문내용
   @Binding var getDate: String // 외부에서 받아오는 날짜
+  @State private var UpSheet: Bool = false
+  @State private var Alert: Bool = false
   
   
   var body: some View {
     // 상단 바
     NavigationView {
-      VStack{}
-        .toolbar{
-          ToolbarItem(placement: .navigationBarLeading){
-            //뒤로가기 버튼
-            Button(action: {print("뒤로 갔습니다")}){
-              Image(systemName:"chevron.backward")
+      
+      VStack(spacing: 35){
+        
+        // 이모티콘 고르기
+       
+          HStack{
+            Text("🩷")
+              .font(.system(size: 50))
+            Text("💔")
+              .font(.system(size: 50))
+            Text("💗")
+              .font(.system(size: 50))
+          }
+        
+        
+        
+        // 일기 작성란
+        VStack {
+          ZStack {
+            TextEditor(text: $comment)
+              .frame(width: 350, height: 400)
+              .padding()
+              .autocorrectionDisabled()
+              .overlay(
+                RoundedRectangle(cornerRadius: 20)
+                  .stroke(Color.gray.opacity(0.25))
+                  .fill(.yellow.opacity(0.1)) //background -> fill or clipshape , 보더 밖으로 튀어나오는 이슈
+              )
+              .font(.body)
+            
+            
+            //가이드 텍스트 표시
+            if comment.isEmpty {
+              VStack {
+                Text("일기를 작성하세요.")
+                  .padding()
+                  .opacity(0.35)
+              }
             }
           }
-          ToolbarItem(placement: .principal){
-            // 선택한 날짜
-            Text(getDate)
-              .font(.system(size: 20))
+        }
+      }
+      
+      // 상단바
+      .toolbar{
+        // 뒤로가기 버튼
+        ToolbarItem(placement: .navigationBarLeading){
+          Button(action: {print("뒤로 갔습니다")}){
+            Image(systemName:"chevron.backward")
           }
         }
-    }
-    .padding()
-    .ignoresSafeArea(edges: .top)
-    
-    // 이모티콘 고르기
-    HStack{
-      Text("🩷")
-        .font(.system(size: 45))
-      Text("💔")
-        .font(.system(size: 45))
-      Text("💗")
-        .font(.system(size: 45))
-    }
-    
-    // 일기 작성란
-    VStack {
-      ZStack {
-        TextEditor(text: $comment)
-          .frame(width: 350, height: 400)
-          .padding()
-          .autocorrectionDisabled()
-          .overlay(
-            RoundedRectangle(cornerRadius: 20)
-              .stroke(Color.gray)
-              .background(.yellow.opacity(0.1))
-          )
-          .font(.body)
-        
-        
-        //가이드 텍스트 표시
-        if comment.isEmpty {
-          VStack {
-            Text("일기를 작성하세요.")
-              .padding()
-              .opacity(0.35)
+        // 선택한 날짜
+        ToolbarItem(placement: .principal){
+          Text(getDate)
+            .font(.system(size: 20))
+        }
+        // 수정&삭제 액션시트
+        ToolbarItem(placement: .navigationBarTrailing){
+          Button(action: {UpSheet = true}){
+            Image(systemName: "ellipsis")
+
+          } //actionsheet -> confirmationDialog로 변경
+          .confirmationDialog("타이틀", isPresented: $UpSheet){
+            Button("수정", action: {})
+            Button("삭제"){
+              Alert = true
+            }
+            .alert("정말 삭제하시겠습니까?", isPresented: $Alert){
+              Button("네"){/*데이터삭제*/}
+            }
           }
         }
       }
-    }
-    
-    
-    
-    //저장 버튼
-    HStack {
-      Spacer()
-      Button(action: {print("일기저장")}) {
-        Image(systemName: "plus.circle.fill")
-          .font(.system(size: 45))
-      }
-      .padding(.trailing, 25)
-      .padding(.top, 15)
     }
   }
+  
 }
+
 
 
 #Preview {
