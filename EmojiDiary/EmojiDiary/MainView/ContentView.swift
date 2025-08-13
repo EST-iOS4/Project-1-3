a394d389ed2478498ea533865ff3e18766e3eb29
algamza246 <algamza246@gmail.com>
2025-08-13 15:05:32 +0900

    feat: re: 설정 버튼 추가 및 위치 정렬

--- EmojiDiary/EmojiDiary/MainView/ContentView.swift
+++ EmojiDiary/EmojiDiary/MainView/ContentView.swift
@@ -1,51 +1,57 @@
 import SwiftUI
 
+
 struct ContentView: View {
     @State private var date = Date()
     
     var body: some View {
         // 첫 번째 화면
-        VStack {
-            ZStack {
-                Text("Project Name")
-                    .font(.headline)
-                    .padding(.horizontal)
-
-                HStack{
-                    Spacer()
+        TabView {
+            // 첫 번째 탭
+            Text("첫 번째 화면")
+            VStack() {
+                ZStack{
+                    Text("Project Name") //Project 제목
+                        .font(.headline)
+                        .padding(.horizontal)
                     
-                    Button(action: {
-                        // 설정 버튼 클릭 시 동작
-                    }) {
-                        Image(systemName: "gearshape")
-                            .imageScale(.large)
+                    HStack{
+                        Spacer()
+                        
+                        Button(action: {
+                            // 설정 버튼 클릭 시 동작
+                        }) {
+                            Image(systemName: "gearshape")
+                                .imageScale(.large)
+                                .padding(.horizontal)
+                        }
                     }
                 }
                 
-                .padding(.horizontal)
+                
+                DatePicker("", selection: $date, displayedComponents: [.date]) //달력
+                    .datePickerStyle(.graphical)
+                    .environment(\.locale, Locale(identifier: "ko")) //yyyy mm 부분 영어 -> 한글
+                    .padding()
+                Spacer()
             }
-
-            DatePicker("", selection: $date, displayedComponents: [.date])
-                .datePickerStyle(.graphical)
-                .environment(\.locale, Locale(identifier: "ko"))
-                .padding()
-
-            Spacer()
-        }
-
                 .tabItem {
                     Image(systemName: "calendar")
                     Text("달력")
                 }
+            
                 
             // 두 번째 탭
             Text("두 번째 화면")
                 .tabItem {
                     Image(systemName: "chart.pie")
                     Text("통계")
+                    
+                    
                 }
         }
     }
+}
 
 #Preview {
     ContentView()
