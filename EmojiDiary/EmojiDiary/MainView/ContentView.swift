a66c6bddd359cc8b212ad63e6b40a12581690667
algamza246 <algamza246@gmail.com>
2025-08-14 02:25:37 +0900

    feat: SettingView 추가, 선택 날짜 범위 지정, 상단 제목, 설정 배치 재조정

--- EmojiDiary/EmojiDiary/MainView/ContentView.swift
+++ EmojiDiary/EmojiDiary/MainView/ContentView.swift
@@ -1,46 +1,16 @@
+//
+//  ContentView.swift
+//  EmojiDiary
+//
+//  Created by 서정원 on 8/12/25.
+//
+
 import SwiftUI
 
-
 struct ContentView: View {
-<<<<<<< HEAD
-    @State private var getDate = Date()
-    
-    var body: some View {
-        NavigationStack {
-            // 첫 번째 화면
-            TabView {
-                // 첫 번째 탭
-                Text("첫 번째 화면")
-
-                VStack() {
-                    ZStack{
-                        Text("Project Name") //Project 제목
-                            .font(.headline)
-                            .padding(.horizontal)
-                        HStack{
-                            Spacer()
-                            
-                            
-                            NavigationLink(destination: SettingView()) {
-                                Image(systemName: "gearshape")
-                                    .imageScale(.large)
-                                    .padding(.horizontal)
-                            }
-                        }
-                    }
-                    
-                    
-                    DatePicker("", selection: $getDate, in: ...Date(), displayedComponents: [.date]) //달력
-                        .datePickerStyle(.graphical)
-                        .environment(\.locale, Locale(identifier: "ko")) //yyyy mm 부분 영어 -> 한글
-                        .padding(.horizontal)
-                    
-                    
-                    Text("선택된 날짜: \(getDate)")
-                    
-=======
     @State private var date = Date()
     @State private var selectedDate = Date()
+    @State private var navigationToSetting = false
     @State private var navigationToDetail = false
     
     var body: some View {
@@ -48,11 +18,26 @@ struct ContentView: View {
         NavigationStack {
             TabView {
                 VStack() {
-                    Text("EmojiDiary") //Project 제목
-                        .font(.headline)
-                        .fontWeight(.bold)
+                    ZStack {
+                        Text("EmojiDiary") //Project 제목
+                            .font(.headline)
+                            .fontWeight(.bold)
+                        HStack {
+                            Spacer()
+                            Button {
+                                navigationToSetting = true
+                            } label: {
+                                Image(systemName: "gearshape")
+                                    .imageScale(.large)
+                                    .padding(.trailing)
+                            }
+                            .navigationDestination(isPresented: $navigationToSetting) {
+                                SettingView()
+                            }
+                        }
+                    }
                     
-                    DatePicker("", selection: $date, displayedComponents: [.date]) //달력
+                    DatePicker("", selection: $date, in: ...Date(),  displayedComponents: [.date]) //달력
                         .datePickerStyle(.graphical)
                         .environment(\.locale, Locale(identifier: "ko")) //yyyy mm 부분 영어 -> 한글
                         .padding()
@@ -63,30 +48,15 @@ struct ContentView: View {
                         .navigationDestination(isPresented: $navigationToDetail) {
                             CreateView(getDate: $date)
                         }
-                        
->>>>>>> 0628f8d525231c7d1cd758ac89c4174bc56412cf
+                    
                     Spacer()
                 }
-                    .tabItem {
-                        Image(systemName: "calendar")
-                        Text("달력")
-                    }
-<<<<<<< HEAD
                 
-                    
-                // 두 번째 탭
-                Text("두 번째 화면")
-                    .tabItem {
-                        Image(systemName: "chart.pie")
-                        Text("통계")
-                        
-                        
-                    }
-            }
-        }
-
-=======
-                    
+                .tabItem {
+                    Image(systemName: "calendar")
+                    Text("달력")
+                }
+                
                 // 두 번째 탭
                 ListView()
                     .tabItem {
@@ -95,11 +65,6 @@ struct ContentView: View {
                     }
             }
         }
-        .navigationDestination(isPresented: $navigationToDetail) {
-            CreateView(getDate: $selectedDate)
-        }
-        
->>>>>>> 0628f8d525231c7d1cd758ac89c4174bc56412cf
     }
 }
 
