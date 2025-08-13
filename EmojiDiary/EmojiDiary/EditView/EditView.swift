fb5d6fb1f030227d49fd54c26e2718c759a7bcca
EunYoung <e.hazel.w@icloud.com>
2025-08-14 01:58:09 +0900

    폴더추가

---
+++ EmojiDiary/EmojiDiary/EditView/EditView.swift
@@ -0,0 +1,18 @@
+//
+//  EditView.swift
+//  EmojiDiary
+//
+//  Created by EunYoung Wang on 8/14/25.
+//
+
+import SwiftUI
+
+struct EditView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EditView()
+}
